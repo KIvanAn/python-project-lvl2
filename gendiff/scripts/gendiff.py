135fa2d23557c22e552761f0d5814d6663161257
Ivan <kozel.vania@yandex.ru>
2022-04-21 22:59:02 +0300

    Task #2 arguments and options

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,13 @@ import argparse
 
 
 def gendiff():
-    parser = argparse.ArgumentParser(description='Generate diff')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument(
+        '-f',
+        '--format',
+        metavar='FORMAT',
+        help='set format of output')
     parser.add_argument('first_file', type=open)
     parser.add_argument('second_file', type=open)
 
